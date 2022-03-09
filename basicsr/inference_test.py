d1a091dfb02626ffe248a13d7f9f1ca4146c6aca
ZhangAoCanada <zhangaocanada@gmail.com>
2022-03-09 08:57:40 +0800

    testing model size

--- basicsr/inference_test.py
+++ basicsr/inference_test.py
@@ -192,7 +192,7 @@ def main():
 
     # create model
     model = create_model(opt)
-    summary(model, (1, 3, 720, 720))
+    summary(model, (1, 3, 512, 512))
 
     for test_loader in test_loaders:
         test_set_name = test_loader.dataset.opt['name']
