182e52cb8530df48f30c1e1af8e237e6db8cf175
ZhangAoCanada <zhangaocanada@gmail.com>
2022-04-15 10:35:09 +0800

    debugging 2070

--- basicsr/utils/misc.py
+++ basicsr/utils/misc.py
@@ -165,7 +165,8 @@ def check_resume(opt, resume_iter):
             if opt['path'].get('ignore_resume_networks') is None or (
                     basename not in opt['path']['ignore_resume_networks']):
                 opt['path'][name] = osp.join(
-                    opt['path']['models'], f'net_{basename}_{resume_iter}.pth')
+                    # opt['path']['models'], f'net_{basename}_{resume_iter}.pth')
+                    opt['path']['models'], f'net_{basename}_latest.pth')
                 logger.info(f"Set {name} to {opt['path'][name]}")
 
 
