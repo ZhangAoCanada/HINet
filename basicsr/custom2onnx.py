4e8a327df37b8ca4a5ee379ed862212e2d7f6b02
ZhangAoCanada <zhangaocanada@gmail.com>
2022-05-26 17:08:21 +0800

    updating

---
+++ basicsr/custom2onnx.py
@@ -0,0 +1,198 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2021 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from BasicSR (https://github.com/xinntao/BasicSR)
+# Copyright 2018-2020 BasicSR Authors
+# ------------------------------------------------------------------------
+import sys
+sys.path.append("/content/drive/MyDrive/DERAIN/HINet")
+import importlib
+import logging
+from unittest import TestLoader
+import torch
+from os import path as osp
+
+from basicsr.data import create_dataloader, create_dataset
+from basicsr.models import create_model
+from basicsr.train import parse_options
+from basicsr.utils import (get_env_info, get_root_logger, get_time_str,
+                           make_exp_dirs)
+from basicsr.utils.options import dict2str
+from basicsr.utils import get_root_logger, imwrite 
+from tqdm import tqdm
+from copy import deepcopy
+import time, cv2
+from skimage.measure import compare_psnr, compare_ssim
+import numpy as np
+from torchvision.utils import make_grid
+import math
+
+from google.colab.patches import cv2_imshow
+
+
+def imfrombytes(content, flag='color', float32=False):
+    """Read an image from bytes.
+
+    Args:
+        content (bytes): Image bytes got from files or other streams.
+        flag (str): Flags specifying the color type of a loaded image,
+            candidates are `color`, `grayscale` and `unchanged`.
+        float32 (bool): Whether to change to float32., If True, will also norm
+            to [0, 1]. Default: False.
+
+    Returns:
+        ndarray: Loaded image array.
+    """
+    img_np = np.frombuffer(content, np.uint8)
+    imread_flags = {
+        'color': cv2.IMREAD_COLOR,
+        'grayscale': cv2.IMREAD_GRAYSCALE,
+        'unchanged': cv2.IMREAD_UNCHANGED
+    }
+    if img_np is None:
+        raise Exception('None .. !!!')
+    img = cv2.imdecode(img_np, imread_flags[flag])
+    if float32:
+        img = img.astype(np.float32) / 255.
+    return img
+
+
+def img2tensor(imgs, bgr2rgb=True, float32=True):
+    """Numpy array to tensor.
+
+    Args:
+        imgs (list[ndarray] | ndarray): Input images.
+        bgr2rgb (bool): Whether to change bgr to rgb.
+        float32 (bool): Whether to change to float32.
+
+    Returns:
+        list[tensor] | tensor: Tensor images. If returned results only have
+            one element, just return tensor.
+    """
+
+    def _totensor(img, bgr2rgb, float32):
+        if img.shape[2] == 3 and bgr2rgb:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = torch.from_numpy(img.transpose(2, 0, 1))
+        img = torch.unsqueeze(img, 0)
+        if float32:
+            img = img.float()
+        return img
+
+    if isinstance(imgs, list):
+        return [_totensor(img, bgr2rgb, float32) for img in imgs]
+    else:
+        return _totensor(imgs, bgr2rgb, float32)
+
+
+
+
+def tensor2img(tensor, rgb2bgr=True, out_type=np.uint8, min_max=(0, 1)):
+    """Convert torch Tensors into image numpy arrays.
+
+    After clamping to [min, max], values will be normalized to [0, 1].
+
+    Args:
+        tensor (Tensor or list[Tensor]): Accept shapes:
+            1) 4D mini-batch Tensor of shape (B x 3/1 x H x W);
+            2) 3D Tensor of shape (3/1 x H x W);
+            3) 2D Tensor of shape (H x W).
+            Tensor channel should be in RGB order.
+        rgb2bgr (bool): Whether to change rgb to bgr.
+        out_type (numpy type): output types. If ``np.uint8``, transform outputs
+            to uint8 type with range [0, 255]; otherwise, float type with
+            range [0, 1]. Default: ``np.uint8``.
+        min_max (tuple[int]): min and max values for clamp.
+
+    Returns:
+        (Tensor or list): 3D ndarray of shape (H x W x C) OR 2D ndarray of
+        shape (H x W). The channel order is BGR.
+    """
+    if not (torch.is_tensor(tensor) or
+            (isinstance(tensor, list)
+             and all(torch.is_tensor(t) for t in tensor))):
+        raise TypeError(
+            f'tensor or list of tensors expected, got {type(tensor)}')
+
+    if torch.is_tensor(tensor):
+        tensor = [tensor]
+    result = []
+    for _tensor in tensor:
+        _tensor = _tensor.squeeze(0).float().detach().cpu().clamp_(*min_max)
+        _tensor = (_tensor - min_max[0]) / (min_max[1] - min_max[0])
+
+        n_dim = _tensor.dim()
+        if n_dim == 4:
+            img_np = make_grid(
+                _tensor, nrow=int(math.sqrt(_tensor.size(0))),
+                normalize=False).numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            # if rgb2bgr:
+            #     img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 3:
+            img_np = _tensor.numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            if img_np.shape[2] == 1:  # gray image
+                img_np = np.squeeze(img_np, axis=2)
+            # else:
+            #     if rgb2bgr:
+            #         img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 2:
+            img_np = _tensor.numpy()
+        else:
+            raise TypeError('Only support 4D, 3D or 2D tensor. '
+                            f'But received with dimension: {n_dim}')
+        if out_type == np.uint8:
+            # Unlike MATLAB, numpy.unit8() WILL NOT round by default.
+            img_np = (img_np * 255.0).round()
+        img_np = img_np.astype(out_type)
+        result.append(img_np)
+    if len(result) == 1:
+        result = result[0]
+        if rgb2bgr:
+            result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
+    return result
+
+
+
+def main():
+    # parse options, set distributed setting, set ramdom seed
+    opt = parse_options(is_train=False)
+
+    torch.backends.cudnn.benchmark = True
+    # torch.backends.cudnn.deterministic = True
+
+    # mkdir and initialize loggers
+    # make_exp_dirs(opt)
+
+    video = cv2.VideoCapture(opt['path']['video_path'])
+
+    # create model
+    model = create_model(opt)
+    net = model.net_g
+
+    # net_path = "experiments/DeRain/models/net_g_best.pth"
+    # net.load_state_dict(torch.load(net_path, map_location=torch.device('cpu')))
+
+    print(net)
+    net.eval()
+
+    with torch.no_grad():
+        while True:
+            ret, frame = video.read()
+            if not ret:
+                break
+            # frame = frame[:, 180:1200, :]
+            frame_tmp = frame.copy().astype(np.float32) / 255.
+            frame_tensor = img2tensor(frame_tmp, bgr2rgb=True, float32=True)
+            pred = net(frame_tensor)
+            # pred = pred.squeeze(0).cpu().numpy()
+            pred_image_cpu = tensor2img(pred, rgb2bgr=True)
+            pred_image_cpu = cv2.resize(pred_image_cpu, (frame.shape[1],frame.shape[0]))
+            image = np.concatenate((frame, pred_image_cpu[..., ::-1]), axis=1)
+            cv2_imshow("image", image)
+
+
+
+if __name__ == '__main__':
+    main()
